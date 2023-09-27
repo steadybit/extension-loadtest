947cba22d0aef6b17585e550518ecb04ce47c169
Ansgar Schulte <1299623+ansgarschulte@users.noreply.github.com>
2023-09-27 10:26:37 +0200

    chore: update dependencies

---
+++ extloadtest/action_do_nothing.go
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2023 steadybit GmbH. All rights reserved.
+ */
+
+package extloadtest
+
+import (
+  "context"
+  "fmt"
+  "github.com/steadybit/action-kit/go/action_kit_api/v2"
+  "github.com/steadybit/action-kit/go/action_kit_sdk"
+  extension_kit "github.com/steadybit/extension-kit"
+  "github.com/steadybit/extension-kit/extbuild"
+  "github.com/steadybit/extension-kit/extconversion"
+  "github.com/steadybit/extension-kit/extutil"
+)
+
+type doNothingAction struct {
+	targetId          string
+	selectionTemplate action_kit_api.TargetSelectionTemplate
+}
+
+// Make sure action implements all required interfaces
+var (
+	_ action_kit_sdk.Action[DoNothingActionState]           = (*doNothingAction)(nil)
+)
+
+type DoNothingActionState struct {
+}
+
+type DoNothingActionConfig struct {
+}
+
+func NewDoNothingAction(targetId string, selectionTemplate action_kit_api.TargetSelectionTemplate) action_kit_sdk.Action[DoNothingActionState] {
+	return &doNothingAction{
+		targetId:          targetId,
+		selectionTemplate: selectionTemplate,
+	}
+}
+
+func (l *doNothingAction) NewEmptyState() DoNothingActionState {
+	return DoNothingActionState{}
+}
+
+func (l *doNothingAction) Describe() action_kit_api.ActionDescription {
+	return action_kit_api.ActionDescription{
+		Id:          fmt.Sprintf("%s.log", l.targetId),
+		Label:       "do nothing",
+		Description: "nothing",
+		Version:     extbuild.GetSemverVersionStringOrUnknown(),
+		TargetSelection: extutil.Ptr(action_kit_api.TargetSelection{
+			TargetType: l.targetId,
+			SelectionTemplates: extutil.Ptr([]action_kit_api.TargetSelectionTemplate{
+				l.selectionTemplate,
+			}),
+		}),
+		Category:    extutil.Ptr("internal"),
+		Kind:        action_kit_api.Attack,
+		TimeControl: action_kit_api.TimeControlInstantaneous,
+		Parameters: []action_kit_api.ActionParameter{
+			{
+				Name:         "duration",
+				Label:        "Duration",
+				Type:         action_kit_api.Duration,
+				DefaultValue: extutil.Ptr("10s"),
+				Required:     extutil.Ptr(true),
+				Order:        extutil.Ptr(0),
+			},
+		},
+	}
+}
+
+func (l *doNothingAction) Prepare(_ context.Context, state *DoNothingActionState, request action_kit_api.PrepareActionRequestBody) (*action_kit_api.PrepareResult, error) {
+	var config DoNothingActionConfig
+	if err := extconversion.Convert(request.Config, &config); err != nil {
+		return nil, extension_kit.ToError("Failed to unmarshal the config.", err)
+	}
+
+	return &action_kit_api.PrepareResult{Messages: extutil.Ptr([]action_kit_api.Message{
+    {
+      Level:   extutil.Ptr(action_kit_api.Info),
+      Message: "Prepared do nothing",
+    },
+  })}, nil
+}
+
+func (l *doNothingAction) Start(_ context.Context, state *DoNothingActionState) (*action_kit_api.StartResult, error) {
+	return &action_kit_api.StartResult{
+		Messages: extutil.Ptr([]action_kit_api.Message{
+			{
+				Level:   extutil.Ptr(action_kit_api.Info),
+				Message: "Started do nothing",
+			},
+		})}, nil
+}
--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.20
 require (
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/procyon-projects/chrono v1.1.2
-	github.com/rs/zerolog v1.30.0
+	github.com/rs/zerolog v1.31.0
 	github.com/steadybit/action-kit/go/action_kit_api/v2 v2.7.3
 	github.com/steadybit/action-kit/go/action_kit_sdk v1.1.7
 	github.com/steadybit/discovery-kit/go/discovery_kit_api v1.4.2
@@ -40,7 +40,7 @@ require (
 	github.com/go-playground/validator/v10 v10.15.4 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
-	github.com/gomarkdown/markdown v0.0.0-20230912175223-14b07df9d538 // indirect
+	github.com/gomarkdown/markdown v0.0.0-20230922112808-5421fefb8386 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/google/uuid v1.3.1 // indirect
 	github.com/gorilla/css v1.0.0 // indirect
@@ -48,13 +48,13 @@ require (
 	github.com/iris-contrib/schema v0.0.6 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/kataras/blocks v0.0.7 // indirect
+	github.com/kataras/blocks v0.0.8 // indirect
 	github.com/kataras/golog v0.1.9 // indirect
-	github.com/kataras/iris/v12 v12.2.6-0.20230908161203-24ba4e8933b9 // indirect
+	github.com/kataras/iris/v12 v12.2.7 // indirect
 	github.com/kataras/pio v0.0.12 // indirect
 	github.com/kataras/sitemap v0.0.6 // indirect
 	github.com/kataras/tunnel v0.0.4 // indirect
-	github.com/klauspost/compress v1.16.7 // indirect
+	github.com/klauspost/compress v1.17.0 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
 	github.com/labstack/echo/v4 v4.11.1 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
@@ -84,6 +84,7 @@ require (
 	github.com/yosssi/ace v0.0.5 // indirect
 	golang.org/x/arch v0.5.0 // indirect
 	golang.org/x/crypto v0.13.0 // indirect
+	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
 	golang.org/x/net v0.15.0 // indirect
 	golang.org/x/sys v0.12.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
